cf0f8b308f4a48d55e1d1a28b5ed24954b791494
Hoda Hamad <hodahamad@aucegypt.edu>
2017-05-16 18:27:26 +0200

    add crud and relations for todos,and add update schema with new migrations

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,39 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+
+    respond_to do |format|
+      format.json {render json: @todos}
+    end
+  end
+
+  def create
+    @todo = Todo.create! todo_params
+    respond_to do |format|
+      format.json {render json:@todo}
+    end
+  end
+
+  def update
+    @todo = Todo.find params[:id]
+    @todo.update todo_params
+
+    respond_to do |format|
+      format.json {render json: @todo}
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    respond_to do |format|
+      format.json {render json: @todo}
+    end
+  end
+
+private
+  def todo_params
+    params.require(:todo).permit(:description, :user_id)
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require "sprockets/railtie"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -28,6 +29,14 @@ module ToDoListRails
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+       allow do
+         origins '*'
+         resource '*',
+           :headers => :any,
+           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+           :methods => [:get, :post, :options, :delete, :put]
+       end
+    end
   end
 end
